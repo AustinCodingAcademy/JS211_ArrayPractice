4f34a11f6f48a983eedc6ecb6caf6bf59f382056
krisrod10 <82917336+krisrod10@users.noreply.github.com>
2021-07-13 21:16:52 -0500

    test

--- main.js
+++ main.js
@@ -39,10 +39,64 @@ const stringofcars = [cars][morecars];
 
 console.log(carcompanys.join(stringofcars));
 
-const words = [stringofcars];
+let carsFromString = 'Ford! Chevy? Dodge. Toyota! Nissan? Hyundai. Kia Honda! '
+let str = carsFromString.split(/[!,?,.]/);
+console.log(str);
 
-const carcarsFromString =.split('Ford');
-console.log(carsfromstring);
+const forward = ['Ford', 'Chevy', 'Dodge', 'Toyota','Nissan','Hyundai','Kia','Honda'];
+console.log('forward:', forward);
+const carsInReverse = forward.reverse();
+console.log('reversed:', forward);
+
+const brands = ['Ford', 'Chevy', 'Dodge', 'Toyota','Nissan','Hyuundai','Kia','Honda'];
+brands.sort();
+console.log(brands);
+
+
+
+const pets = ['dog', 'cat', 'fish', 'rabbit', 'snake','lizard','bird'];
+
+console.log(pets.slice(4, 6));
+
+const removedPet = ['Dog', 'cat', 'fish', 'rabbit', 'snake','lizard','bird'];
+
+console.log(removedPet.pop());
+
+const animals = ['Dog', 'cat', 'fish','rabbit','snake','lizard'];
+const animalName = animals.push('bird');
+
+console.log(animalName);
+
+console.log(animals);
+
+animals.push('bird');
+
+console.log(animals);
+
+const pets2 = ['Dog','cat','fish','rabbit','snake','lizard','bird'];
+
+const firstElement = pets2.shift();
+
+console.log(firstElement);
+// expected output: Array [2, 3]
+
+const array4 = ['Dog','cat','fish','rabbit','snake','lizard','bird'];
+
+console.log(array4.unshift('turtle'));
+
+
+console.log(array4);
+
+const numbers = [23, 45, 0 , 2, 8, 44, 100, 1, 3, 91, 34];
+const copyItems = []
+numbers.forEach(function(numbers){
+    copyItems.push(numbers * 10)
+  })
+  
+
+
+
+
 
 
 
